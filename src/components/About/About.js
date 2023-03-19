dc2f0420e85c192cb316e049fdd6088d0b554eff
Bhargavi Reddy <bhargavi.attapuram@sailotech.net>
2023-03-19 20:49:35 +0530

    dev

---
+++ src/components/About/About.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { stackList } from "../../data/ProjectData";
+import {
+  Image,
+  Technologies,
+  Tech,
+  TechImg,
+  TechName,
+  ContactWrapper,
+} from "./AboutElements";
+function About() {
+  return (
+    <ContactWrapper id="about">
+      <div className="Container">
+        <div className="SectionTitle">About Me</div>
+        <div className="BigCard">
+          <Image
+            src="https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/man-svgrepo-com.svg"
+            alt="man-svgrepo"
+          />
+          <div className="AboutBio">
+            Hello! My name is <strong>Bhargavi.</strong> I'm a Software Developer
+            specialized in building web applications using React. Proficient in 
+            JavaScript, HTML, and CSS and having a strong understanding of web development principles and concepts. 
+            Experienced in using tools and technologies such as Redux, React Router, and Axios to build scalable and performant web applications.
+            Also playing a critical role in developing modern web applications that are responsive, interactive, and user-friendly.
+          </div>
+          <div className="AboutBio tagline2">
+            I have become confident using the following technologies.
+          </div>
+          <Technologies>
+            {stackList.map((stack, index) => (
+              <Tech key={index} className="tech">
+                <TechImg src={stack.img} alt={stack.name} />
+                <TechName>{stack.name}</TechName>
+              </Tech>
+            ))}
+          </Technologies>
+        </div>
+      </div>
+    </ContactWrapper>
+  );
+}
+
+export default About;
